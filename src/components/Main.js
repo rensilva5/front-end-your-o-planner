ebd87d15b87eaa4b1873ee1aff796957ed7e0d4d
rensilva5 <renzoasilva@gmail.com>
2022-08-30 22:47:01 -0400

    working on my db

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,25 @@
+import { useEffect } from "react";
+
+
 const Main = () => {
+
+    useEffect(() => {
+        fetch("https://deploy-api-your-o-planner.web.app/main")
+            .then((result) => result.json())
+            .then((tasks) => setTasklist(tasks))
+            .catch(console.error)
+    }, [setTasklist])
+    if (!setTasklist) {
+        return <h2>No tasks done yet</h2>
+    }
     return (
-        <h3>Getting my Main working also</h3>
+        <ul>
+            {tasklist.map((task) => (
+                <li key={task.id}>{task.task}
+                </li>
+            ))}
+        </ul>
+        // <h3>Getting my Main working also</h3>
     )
 }
  
