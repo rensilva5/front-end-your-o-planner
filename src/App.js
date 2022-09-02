136255e0f17c6646ac68c60fffde46f590b347a8
rensilva5 <renzoasilva@gmail.com>
2022-09-01 21:38:43 -0400

    Deployed and tested

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import './App.css';
 
 function App() {
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,26 @@
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
 
 const Main = () => {
+  const [taskDestination, setDestinationList] = useState("")
+  useEffect(() => {
+    fetch("https://deploy-api-your-o-planner.web.app/main")
+      .then((result) => result.json())
+      .then((destinations) => setDestinationList(destinations))
+      .catch(console.error);
+  }, [setDestinationList]);
+  if (!setDestinationList) {
+    return <h2>No tasks done yet</h2>;
+  }
+  return (
+    <ul>
+      {destinationList.map((destination) => (
+        <li key={destination.id}>{destination.task}</li>
+      ))}
+    </ul>
+    // <h3>Getting my Main working also</h3>
+  )
+};
 
-    useEffect(() => {
-        fetch("https://deploy-api-your-o-planner.web.app/main")
-            .then((result) => result.json())
-            .then((tasks) => setTasklist(tasks))
-            .catch(console.error)
-    }, [setTasklist])
-    if (!setTasklist) {
-        return <h2>No tasks done yet</h2>
-    }
-    return (
-        <ul>
-            {tasklist.map((task) => (
-                <li key={task.id}>{task.task}
-                </li>
-            ))}
-        </ul>
-        // <h3>Getting my Main working also</h3>
-    )
-}
- 
 export default Main;
 
 // https://deploy-api-your-o-planner.web.app/test
